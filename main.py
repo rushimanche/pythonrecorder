c7ad933b28f19c69cb866e4ab5a9de1fea77d138
rushimanche <61168471+rushimanche@users.noreply.github.com>
2021-05-12 10:57:58 -0400

    added changes for microphone adaptation

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import struct
 import wave
 import time
 import os
+import sys
 
 #this is where your audio files will be stored. Please change this to meet your liking.
 directory = r'C:\Users\dunka\Documents\GitHub\audioproject\directory'
@@ -26,10 +27,11 @@ RATE = 16000
 swidth = 2
 
 #customize this value based on background noise. The higher the value, the louder the sound must be to be considered a sound.
-maxSoundVal = 50
+maxSoundVal = 10
 
 #Interval AFTER sound finishes that script waits. Don't make this zero.
 pauseInterval = 1
+
 
 class SoundRecorder:
 
@@ -51,12 +53,31 @@ class SoundRecorder:
 
     def __init__(self):
         self.p = pyaudio.PyAudio()
+        info = self.p.get_host_api_info_by_index(0)
+        numdevices = info.get('deviceCount')
+        device_name = 'Microphone Array (Realtek(R) Au'
+        device_id = 0
+
+        for i in range(0, numdevices):
+            if (self.p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+                print("Input Device ID:", i, " - ", self.p.get_device_info_by_host_api_device_index(0, i).get('name'))
+
+        for i in range(0, numdevices):
+            audio_names = []
+            if (self.p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+                if(self.p.get_device_info_by_host_api_device_index(0, i).get('name') == device_name):
+                    device_id = i
+
+        if device_id == 0:
+            sys.exit("Audio device not found! Please modify device_name variable.")
+
         self.stream = self.p.open(format=FORMAT,
-                                  channels=CHANNELS,
-                                  rate=RATE,
-                                  input=True,
-                                  output=True,
-                                  frames_per_buffer=chunk)
+                                input_device_index=device_id,
+                                channels=CHANNELS,
+                                rate=RATE,
+                                input=True,
+                                output=True,
+                                frames_per_buffer=chunk)
 
     #function responsible for listening to sound
     def listenForSound(self):
@@ -69,7 +90,7 @@ class SoundRecorder:
                 self.recordSound()
 
     #function responsible for recording sound
-    def recordSound(self):
+    def recordSound(self):a
         print('Noise sensed, recording is beginning. To stop the recording, stop noise.')
         currentSoundLevel = []
         current = time.time()
@@ -77,9 +98,7 @@ class SoundRecorder:
 
         while current <= end:
             data = self.stream.read(chunk)
-            print(data)
             if self.calculator(data) >= maxSoundVal: end = time.time() + pauseInterval
-
             current = time.time()
             currentSoundLevel.append(data)
         self.save(b''.join(currentSoundLevel))
